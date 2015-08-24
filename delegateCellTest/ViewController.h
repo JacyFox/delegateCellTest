c50628c9c9542c3f3ef7e1048961bc25efa2c5db
郇建忠 <huanjianzhong@lakala.com>
2015-08-24 14:21:29 +0800

    Initial Commit

---
+++ delegateCellTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  delegateCellTest
+//
+//  Created by lakala on 15/8/24.
+//  Copyright (c) 2015年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ delegateCellTestTests/delegateCellTestTests.m
@@ -0,0 +1,40 @@
+//
+//  delegateCellTestTests.m
+//  delegateCellTestTests
+//
+//  Created by lakala on 15/8/24.
+//  Copyright (c) 2015年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface delegateCellTestTests : XCTestCase
+
+@end
+
+@implementation delegateCellTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
